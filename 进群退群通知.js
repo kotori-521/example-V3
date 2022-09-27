d489e4ce8975a668cc888a59e7319d16ae591d1a
南 ことり <104956864+kotori-521@users.noreply.github.com>
2022-09-27 08:11:00 +0800

    Update 进群退群通知.js

--- 进群退群通知.js
+++ 进群退群通知.js
@@ -14,7 +14,7 @@ export class newcomer extends plugin {
   /** 接受到消息都会执行一次 */
   async accept () {
     /** 定义入群欢迎内容 */
-    let msg = '欢迎新人！'
+    let msg = '欢迎大佬加入！我是纳西妲，一个原神机器人，如需查看使用方法，可以发送“帮助”哦，纳西妲很喜欢你呢！'
     /** 冷却cd 30s */
     let cd = 30
 
