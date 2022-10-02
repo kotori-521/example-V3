b5d4102989072b9119a43011a0dbbdaf4fb5be2c
南 ことり <104956864+kotori-521@users.noreply.github.com>
2022-10-02 23:30:08 +0800

    Update more-js-V3.js

--- more-js-V3.js
+++ more-js-V3.js
@@ -60,7 +60,7 @@ export class more_js extends plugin {
   //e.msg 用户的命令消息
   console.log("用户命令：", e.msg);
   //执行的逻辑功能
-  let url = `https://api.vvhan.com/api/avatar`;
+  let url = `https://img.xjh.me/random_img.php`;
   
   let msg = [ 
     segment.at(e.user_id),  
