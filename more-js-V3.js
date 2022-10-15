7f9b390299e4d75524a4e0a0f4060f64472a4b3a
南 ことり <104956864+kotori-521@users.noreply.github.com>
2022-10-15 16:39:42 +0800

    Update more-js-V3.js

--- more-js-V3.js
+++ more-js-V3.js
@@ -61,7 +61,8 @@ export class more_js extends plugin {
   console.log("用户命令：", e.msg);
   //执行的逻辑功能
   let url = `https://api.uomg.com/api/rand.avatar?sort=动漫女&format=image`;
- let res = await fetch(url).catch((err) => logger.error(err));
+
+	 let res = await fetch(url).catch((err) => logger.error(err));
         
         let msg = [segment.at(e.user_id), segment.image(res.url)];
   //发送消息
